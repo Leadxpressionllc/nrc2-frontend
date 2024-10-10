33dc4bdbad498e8a9941fadda0bcf53f99fa7a1e
Zee <zulqar9kamboh@gmail.com>
2024-10-10 12:35:03 +0500

    basic layout and homepage is added

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -3,3 +3,5 @@ export * from './form-control/form-control.component';
 export * from './new-build-notification/new-build-notification.component';
 export * from './popup/dialogbase.directive';
 export * from './popup/popup.component';
+export * from './footer/footer.component';
+export * from './header/header.component';
