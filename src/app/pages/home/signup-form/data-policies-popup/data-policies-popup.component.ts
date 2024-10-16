21128cd11073d2da89ada2f8067edf8d59244758
Mukesh Kumar <mukeshkumar@greenlambda.com>
2024-10-16 17:14:07 +0500

    data-policies-popup added

---
+++ src/app/pages/home/signup-form/data-policies-popup/data-policies-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrc-data-policies-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-policies-popup.component.html',
+  styleUrl: './data-policies-popup.component.scss',
+})
+export class DataPoliciesPopupComponent {}
