ff244b73c2af72053202c17e4889b3ff919fb181
Mukesh Kumar <mukeshkumar@greenlambda.com>
2024-10-31 15:54:33 +0500

    Other Domain Data integrated

--- src/app/core/resolvers/domain-info.resolver.ts
+++ src/app/core/resolvers/domain-info.resolver.ts
@@ -2,9 +2,11 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { DomainInfo } from '@core/models';
 import { CommonService } from '@core/services';
+import { AppService } from '@core/utility-services';
 import { Observable } from 'rxjs';
 
 export const domainInfoResolveFn: ResolveFn<any> = (): Observable<DomainInfo> => {
   const commonService = inject(CommonService);
-  return commonService.getDomainInfoByDomain(window.location.hostname);
+  const domain: string = window.location.ancestorOrigins[0];
+  return commonService.getDomainInfoByDomain(domain);
 };
